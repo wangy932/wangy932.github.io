39b416a99b35b87140a06474cb0f86e9077c7f72
Yuqi Wang <wangy932@newschool.edu>
2017-02-28 09:19:13 -0500

    Initial commit

---
+++ a_collection/assets/js/script.js
@@ -0,0 +1,12 @@
+var home = document.getElementById("home");
+	tour = document.getElementById("tour");
+	music = document.getElementById("music");
+	video = document.getElementById("video");
+	merch = document.getElementById("merch");
+	jackTour = document.getElementById("jack_tour");
+	jackMusic = document.getElementById("jack_music");
+	jackVideo = document.getElementById("jack_video");
+	jackMerch = document.getElementById("jack_merch");
+
+jackTour.addEventListener("click");
+
