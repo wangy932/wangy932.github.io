bddc6b3ef2eca0ba6e93e44bf06d9a07f414c51f
Yuqi Wang <wangy932@newschool.edu>
2017-05-06 12:47:40 -0400

    initial commit

--- blog/assets/js/test.js
+++ blog/assets/js/test.js
@@ -7,23 +7,11 @@ audioElement1.addEventListener("canplay", function() {
     source1.connect(analyser1);
     analyser1.connect(context1.destination);
 });
-analyser1.fftSize = 256;
+analyser1.fftSize = 1024;
 var frequencyData1 = new Uint8Array(analyser1.frequencyBinCount);
 analyser1.getByteFrequencyData(frequencyData1);
 var circle1 = document.getElementById("v1");
 var b1 = document.getElementById("b1");
-b1.addEventListener("click", function() {
-    if (audioElement1.paused) {
-        audioElement1.play();
-        update1();
-        audioElement2.play();
-        audioElement1.volume = 0.7;
-        update2();
-    } else {
-        audioElement1.pause();
-        audioElement2.pause();
-    }
-})
 
 
 
@@ -37,7 +25,7 @@ audioElement2.addEventListener("canplay", function() {
     source2.connect(analyser2);
     analyser2.connect(context2.destination);
 });
-analyser2.fftSize = 256;
+analyser2.fftSize = 1024;
 var frequencyData2 = new Uint8Array(analyser2.frequencyBinCount);
 analyser2.getByteFrequencyData(frequencyData2);
 var circle2 = document.getElementById("v2");
@@ -68,8 +56,8 @@ function update1() {
     total2 += frequencyData2[i];
   }
   var mean = (total1-total2)/frequencyData1.length;
-  circle1.style.width = Math.round(mean*3) + "px";
-  circle1.style.height = Math.round(mean*3) + "px";
+  circle1.style.width = mean*2 + "px";
+  circle1.style.height = mean*2 + "px";
 };
 
 function update2() {
@@ -85,6 +73,19 @@ function update2() {
     total += frequencyData2[i];
   }
   var mean = total/frequencyData2.length;
-  circle2.style.width = Math.round(mean*3) + "px";
-  circle2.style.height = Math.round(mean*3) + "px";
+  circle2.style.width = mean*2 + "px";
+  circle2.style.height = mean*2 + "px";
 };
+
+b1.addEventListener("click", function() {
+    if (audioElement1.paused) {
+        audioElement1.play();
+        update1();
+        audioElement2.volume = 0.9;
+        audioElement2.play();
+        update2();
+    } else {
+        audioElement1.pause();
+        audioElement2.pause();
+    }
+})
