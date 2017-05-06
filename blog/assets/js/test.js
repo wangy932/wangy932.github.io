caf1524cc55d5e1ccbd0c4174862461f156da2b3
Yuqi Wang <wangy932@newschool.edu>
2017-05-06 12:01:01 -0400

    initial commit

--- blog/assets/js/test.js
+++ blog/assets/js/test.js
@@ -1,6 +1,6 @@
-var audio = document.getElementById("song");
+
 var context1 = new AudioContext();
-var audioElement1 = document.getElementById("song1");
+var audioElement1 = document.getElementById("song");
 var analyser1 = context1.createAnalyser();
 audioElement1.addEventListener("canplay", function() {
     var source1 = context1.createMediaElementSource(audioElement1);
@@ -15,36 +15,22 @@ var b1 = document.getElementById("b1");
 b1.addEventListener("click", function() {
     if (audioElement1.paused) {
         audioElement1.play();
-        audioElement1.muted = true;
-        audio.play();
         update1();
+        audioElement2.play();
+        audioElement1.volume = 0.7;
+        update2();
     } else {
         audioElement1.pause();
-        audio.pause();
+        audioElement2.pause();
     }
 })
 
-function update1() {
-    // Schedule the next update
-    requestAnimationFrame(update1);
 
-    // Get the new frequency data
-    analyser1.getByteFrequencyData(frequencyData1);
-
-    // Update the visualisation
-    var total = 0;
-  for (var i = 0; i < frequencyData1.length; i ++) {
-    total += frequencyData1[i];
-  }
-  var mean = total/frequencyData1.length;
-  circle1.style.width = Math.round(mean*3) + "px";
-  circle1.style.height = Math.round(mean*3) + "px";
-};
 
 // Kick it off...
 
 var context2 = new AudioContext();
-var audioElement2 = document.getElementById("song2");
+var audioElement2 = document.getElementById("instrumental");
 var analyser2 = context2.createAnalyser();
 audioElement2.addEventListener("canplay", function() {
     var source2 = context2.createMediaElementSource(audioElement2);
@@ -57,9 +43,34 @@ analyser2.getByteFrequencyData(frequencyData2);
 var circle2 = document.getElementById("v2");
 var b2 = document.getElementById("b2");
 b2.addEventListener("click", function() {
-    audioElement2.play();
-    update2();
+    if (audioElement2.paused) {
+        audioElement2.play();
+        update1();
+    } else {
+        audioElement2.pause();
+    }
 })
+
+function update1() {
+    // Schedule the next update
+    requestAnimationFrame(update1);
+
+    // Get the new frequency data
+    analyser1.getByteFrequencyData(frequencyData1);
+
+    // Update the visualisation
+    var total1 = 0;
+    var total2 = 0;
+  for (var i = 0; i < frequencyData1.length; i ++) {
+    total1 += frequencyData1[i];
+  }
+  for (var i = 0; i < frequencyData2.length; i ++) {
+    total2 += frequencyData2[i];
+  }
+  var mean = (total1-total2)/frequencyData1.length;
+  circle1.style.width = Math.round(mean*3) + "px";
+  circle1.style.height = Math.round(mean*3) + "px";
+};
 
 function update2() {
     // Schedule the next update
